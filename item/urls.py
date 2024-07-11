31225d9d63157f82ee6499e4cfe2eb732d6aaf5d
Krste <toshev.krste@yahoo.com>
2024-07-11 14:43:22 +0200

    Add edit item action

--- item/urls.py
+++ item/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('new/', views.new_item, name='new_item'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete_item, name='delete_item'),
+    path('<int:pk>/edit/', views.edit_item, name='edit_item'),
 ]
--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 
 def detail(request, pk):
@@ -33,6 +33,27 @@ def new_item(request):
     return render(request=request, template_name='item/form.html', context={
         'form': form,
         'title': 'New item'
+    })
+
+
+@login_required
+def edit_item(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request=request, template_name='item/form.html', context={
+        'form': form,
+        'title': 'Edit item'
     })
 
 
